54905fa2efb74d425d1ee46f83726854403bdc46
Mixwinstonwell <zhaoweicheng0824@gmail.com>
2022-10-01 15:53:37 -0700

    Fixed Command prompt toggle show/hide

--- Pong Assignment 1/Assets/Scripts/CommandPrompt.cs
+++ Pong Assignment 1/Assets/Scripts/CommandPrompt.cs
@@ -1,26 +1,42 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class CommandPrompt : MonoBehaviour
-//{
-//    public GameObject console;
+public class CommandPrompt : MonoBehaviour
+{
+    public GameObject console;
 
-//    public void Update()
-//    {
-//        if (Input.GetKeyDown(KeyCode.C))
-//        {
-//            openConsole();
-//        }
-//    }
+    public void Update()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            //Instantiate(console);
+            openConsole();
+            Debug.Log("Open");
+            
+        }
 
-//    public void openConsole()
-//    {
-//        console.SetActive(true);
-//    }
+    }
 
-//    public void exit()
-//    {
-//        console.SetActive(false);
-//    }
-//}
+    public void openConsole()
+    {
+        console.GetComponent<Renderer>().enabled = true;
+        for (int a = 0; a < transform.childCount; a++)
+        {
+            transform.GetChild(a).gameObject.SetActive(true);
+        }
+
+    }
+
+    public void exit()
+    {
+        //console.SetActive(false);
+        console.GetComponent<Renderer>().enabled = false;
+        for (int a = 0; a < transform.childCount; a++)
+        {
+            transform.GetChild(a).gameObject.SetActive(false);
+        }
+    }
+
+
+}
