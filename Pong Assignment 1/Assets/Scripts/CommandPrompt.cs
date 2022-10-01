d4265d5d645408c8a7e4f5c505c60b6056675adc
gurjotmander <60756988+gurjotmander@users.noreply.github.com>
2022-10-01 14:07:48 -0700

    created console

---
+++ Pong Assignment 1/Assets/Scripts/CommandPrompt.cs
@@ -0,0 +1,26 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class CommandPrompt : MonoBehaviour
+//{
+//    public GameObject console;
+
+//    public void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.C))
+//        {
+//            openConsole();
+//        }
+//    }
+
+//    public void openConsole()
+//    {
+//        console.SetActive(true);
+//    }
+
+//    public void exit()
+//    {
+//        console.SetActive(false);
+//    }
+//}
