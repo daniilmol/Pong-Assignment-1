1c7f9d6ac997a99e18f9cebfec3a735fec81df36
daniilmol <56126113+daniilmol@users.noreply.github.com>
2022-10-04 15:16:13 -0700

    Fixed singleplayer scoring

--- Pong Assignment 1/Assets/Scripts/Paddle Controllers/PaddleControllerTwo.cs
+++ Pong Assignment 1/Assets/Scripts/Paddle Controllers/PaddleControllerTwo.cs
@@ -8,22 +8,26 @@ using TMPro;
 public class PaddleControllerTwo : PaddleController
 {
     public TextMeshProUGUI redScore;
+    private float position;
     private void Start()
     {
-        if (StartGame.sg.singleFlag)
-        {
-            disableMultiMode();
-            return;
-        }
         playerID = 2;
         score = 0;
         redScore.text = 0.ToString();
     }
     private void FixedUpdate()
     {
-        Vector2 v2 = movement2.ReadValue<Vector2>();
-        Vector3 v3 = new Vector3(0, v2.y, 0);
-        transform.Translate(v3 * 0.05f);
+        if (StartGame.sg.mutilFlag) {
+            Vector2 v2 = movement2.ReadValue<Vector2>();
+            Vector3 v3 = new Vector3(0, v2.y, 0);
+            transform.Translate(v3 * 0.05f);
+        }
+        if (StartGame.sg.singleFlag) {
+            position = FindObjectOfType<Ball>().ballYLocation;
+            Vector3 v3 = new Vector3(0, position, 0);
+            transform.Translate(v3 * 0.05f);
+        }
+
         redScore.text = getScore().ToString();
         checkWin();
     }
