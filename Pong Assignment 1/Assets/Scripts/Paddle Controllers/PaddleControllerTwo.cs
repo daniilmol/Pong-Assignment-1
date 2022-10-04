da01d7a25a1652e9088690aba6ccf462a0fc85ef
daniilmol <56126113+daniilmol@users.noreply.github.com>
2022-10-04 15:28:07 -0700

    refactored playercontrollertwo.cs

--- Pong Assignment 1/Assets/Scripts/Paddle Controllers/PaddleControllerTwo.cs
+++ Pong Assignment 1/Assets/Scripts/Paddle Controllers/PaddleControllerTwo.cs
@@ -17,23 +17,29 @@ public class PaddleControllerTwo : PaddleController
     }
     private void FixedUpdate()
     {
-        if (StartGame.sg.mutilFlag) {
-            Vector2 v2 = movement2.ReadValue<Vector2>();
-            Vector3 v3 = new Vector3(0, v2.y, 0);
-            transform.Translate(v3 * 0.05f);
-        }
-        if (StartGame.sg.singleFlag) {
-            position = FindObjectOfType<Ball>().ballYLocation;
-            Vector3 v3 = new Vector3(0, position, 0);
-            transform.Translate(v3 * 0.05f);
-        }
-
+        AIControl();
+        PlayerControl();
         redScore.text = getScore().ToString();
         checkWin();
     }
 
-    private void disableMultiMode()
+    private void AIControl() 
     {
-        this.enabled = false;
+        if (StartGame.sg.singleFlag)
+        {
+            position = FindObjectOfType<Ball>().ballYLocation;
+            Vector3 v3 = new Vector3(0, position, 0);
+            transform.Translate(v3 * 0.05f);
+        }
+    }
+
+    private void PlayerControl()
+    {
+        if (StartGame.sg.mutilFlag)
+        {
+            Vector2 v2 = movement2.ReadValue<Vector2>();
+            Vector3 v3 = new Vector3(0, v2.y, 0);
+            transform.Translate(v3 * 0.05f);
+        }
     }
 }
